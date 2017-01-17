d9e6cfb3f18c16e73e6e4c20c4222699e2e08ce7
Mark Cassidy <mark@cassidy.dk>
2017-01-17 18:45:13 +0100

    Various Rules Engine conditions added

--- RulesEngine/VariableSectionsAllowed.cs
+++ RulesEngine/VariableSectionsAllowed.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
+using Sitecore.Collections;
+using Sitecore.Rules;
+using Sitecore.Rules.Conditions;
 
 namespace Website.RulesEngine
 {
-	public class VariableSectionsAllowed
+	public class VariableSectionsAllowed<T> : IntegerComparisonCondition<T> where T : RuleContext
 	{
+		protected override bool Execute(T ruleContext)
+		{
+			return Compare(ruleContext.Item.GetChildren(ChildListOptions.IgnoreSecurity).Count(i => i.TemplateName.Equals("Section Page")));
+		}
 	}
 }
---
+++ RulesEngine/VariableTemplatesAllowed.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Sitecore.Collections;
+using Sitecore.Rules;
+using Sitecore.Rules.Conditions;
+
+namespace Website.RulesEngine
+{
+	public class VariableTemplatesAllowed<T> : IntegerComparisonCondition<T> where T : RuleContext
+	{
+		public string TemplateId { get; set; }
+
+		protected override bool Execute(T ruleContext)
+		{
+			return Compare(ruleContext.Item.GetChildren(ChildListOptions.IgnoreSecurity).Count(i => i.TemplateID.ToString().Equals(TemplateId)));
+		}
+	}
+}
