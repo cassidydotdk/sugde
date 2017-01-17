890c805197a4c8f92fe54aaab2d3dba19192870b
Mark Cassidy <mark@cassidy.dk>
2017-01-17 17:56:06 +0100

    Max Sections Allowed Rule Condition

---
+++ RulesEngine/MaxSectionsAllowed.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using Sitecore.Collections;
+using Sitecore.Rules;
+using Sitecore.Rules.Conditions;
+
+namespace Website.RulesEngine
+{
+	public class MaxSectionsAllowed<T> : WhenCondition<T> where T : RuleContext
+	{
+		protected override bool Execute(T ruleContext)
+		{
+			return ruleContext.Item.GetChildren(ChildListOptions.IgnoreSecurity).Count(i => i.TemplateName.Equals("Section Page")) < 5;
+		}
+	}
+}
---
+++ RulesEngine/VariableSectionsAllowed.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.RulesEngine
+{
+	public class VariableSectionsAllowed
+	{
+	}
+}
